c14192eab16ec19731cca20c80fc08bda5cd6d41
len smith <ignu.smith@gmail.com>
2009-12-23 10:41:41 -0500

    shuffle and shift, broken combinations

--- src/ruebee/EnumerableExtensions.cs
+++ src/ruebee/EnumerableExtensions.cs
@@ -17,5 +17,52 @@ namespace ruebee
         {
             return value.Distinct();
         }
+
+        public static T Shift<T>(this IList<T> collection)
+        {
+            var rv = collection.First();
+            collection.RemoveAt(0);
+            return rv;
+        }
+
+        public static void Shuffle<T>(this IList<T> collection)
+        {
+            var rand = new Random(DateTime.Now.Millisecond);
+            0.UpTo(collection.Count -1, i=>
+                                         {
+                                             var nextIndex = rand.Next(collection.Count - 1);
+                                             var temp = collection[i];
+                                             collection[i] = collection[nextIndex];
+                                             collection[nextIndex] = temp;
+                                         });
+        }
+
+        public static List<List<T>> Combination<T>(this IEnumerable<T> value, int combinationSize)
+        {
+            var listToReturn = new List<List<T>>();
+            getCombination(listToReturn, new List<T>(), new List<T>(value), combinationSize);
+            return listToReturn;
+        }
+
+        private static void getCombination<T>(List<List<T>> listToReturn, List<T> currentList, List<T> remainingCollection, int combinationSize)
+        {
+
+            if(remainingCollection.Count() < 1) return;
+            Console.WriteLine("Getting Combination " + remainingCollection.First());            
+            var currentItem = remainingCollection.Shift();
+            var nextLevel = new List<T>(remainingCollection);
+            while(remainingCollection.Count() > 0)
+            {
+                if(currentList.Count() ==0) currentList.Add(currentItem);                
+                currentList.Add(remainingCollection.Shift());
+                if (currentList.Count == combinationSize)
+                {
+                    listToReturn.Add(currentList);
+                    currentList = new List<T>();
+                }
+            }
+            getCombination(listToReturn, new List<T>(currentList), nextLevel, combinationSize);
+            
+        }
     }
 }
--- src/specs/EnumerableSpecs.cs
+++ src/specs/EnumerableSpecs.cs
@@ -12,9 +12,14 @@ namespace Ruebee.Specs
         [Test]
         public void can_perform_action_on_each()
         {
+            var assembly = System.Reflection.Assembly.GetAssembly(typeof (EnumerableSpecs));
+            assembly.GetTypes().Each(t=>t.GetFields().Each(Console.WriteLine));
+
             var myString = String.Empty;
             "abcd".Each(a=>myString+=a);
             myString.ShouldEqual("abcd");
+            var ints = new List<int> {1, 2, 3};
+            ints.Each(Console.WriteLine);            
         }
 
         [Test]
@@ -23,6 +28,47 @@ namespace Ruebee.Specs
             var ints = new List<int> {2, 3, 4, 2, 3,};
             ints.Uniq().Count().ShouldEqual(3);
             ints.Uniq().Last().ShouldEqual(4);            
+        }
+
+        [Test]
+        public void can_shift()
+        {
+            var ints = new List<int> {1, 2, 3, 4};
+            var x = ints.Shift();
+            x.ShouldEqual(1);
+            ints.Count().ShouldEqual(3);
+            ints.First().ShouldEqual(2);                
+        }
+
+        [Test]
+        public void can_shuffle()
+        {            
+            var ints = new List<int>();
+            1.UpTo(500, ints.Add);
+            ints.Shuffle();            
+            (ints[0] == 1 && ints[1] == 2 && ints.Last() == 500).ShouldBeFalse();
+            ints.Distinct().Count().ShouldEqual(500);         
+        }
+
+        [Test, Ignore]
+        public void can_get_combinations()
+        {
+            var ints = new List<int> { 1, 2, 3, 4 };
+            var combinations = ints.Combination(2);
+            
+            combinations.Count().ShouldEqual(6);
+            combinations.Last().First().ShouldEqual(3);
+            combinations.Last().Last().ShouldEqual(4);
+            combinations.First().First().ShouldEqual(1);
+            combinations.First().Last().ShouldEqual(2);
+
+            combinations = ints.Combination(3);
+            combinations.Each(c => { c.Each(Console.Write); Console.WriteLine("------------"); });
+            combinations.Count().ShouldEqual(4);
+            combinations.First().First().ShouldEqual(1);
+            combinations.Last().Last().ShouldEqual(3);
+            combinations.Last().First().ShouldEqual(2);
+            combinations.Last().Last().ShouldEqual(4);
         }
     }
 }
